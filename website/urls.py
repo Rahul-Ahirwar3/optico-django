f99dfbd64159d62a7a935d3cf9da4444efb174d9
Rahul Ahirwar <ra362176@gmail.com>
2026-08-08 12:58:49 +0530

    Initial commit

---
+++ website/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.home, name='home'),
+
+    path('about/', views.about, name='about'),
+
+    path('products/', views.products, name='products'),
+
+    path('contact/', views.contact, name='contact'),
+
+    path('login/', views.login_view, name='login'),
+
+]
---
+++ website/views.py
@@ -0,0 +1,45 @@
+
+from django.shortcuts import render, redirect
+from .models import Contact
+
+
+# Home Page
+def home(request):
+    return render(request, 'home.html')
+
+
+# About Page
+def about(request):
+    return render(request, 'about.html')
+
+
+# Products Page
+def products(request):
+    return render(request, 'products.html')
+
+
+# Contact Page
+def contact(request):
+
+    if request.method == "POST":
+
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        Contact.objects.create(
+            name=name,
+            email=email,
+            phone=phone,
+            message=message
+        )
+
+        return redirect('contact')
+
+    return render(request, 'contact.html')
+
+
+# Login Page
+def login_view(request):
+    return render(request, 'login.html')
