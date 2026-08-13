cfad4df98ecf626dbacdc1919898e41732e854c8
Rahul Ahirwar <ra362176@gmail.com>
2026-08-13 13:34:12 +0530

    Fix Cloudinary product images

--- website/models.py
+++ website/models.py
@@ -1,18 +1,5 @@
-# from django.db import models
-
-
-# class Contact(models.Model):
-#     name = models.CharField(max_length=100)
-#     email = models.EmailField()
-#     phone = models.CharField(max_length=15)
-#     message = models.TextField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-#         return self.name
-
-
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 class Contact(models.Model):
@@ -29,7 +16,7 @@ class Contact(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='products/')
+    image = CloudinaryField('image')
 
     def __str__(self):
         return self.name
